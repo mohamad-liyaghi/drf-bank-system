e565f4d2fbf96be6e8b4732f93dd510e3cc09c1e
Ml06py <liaghimohamad69@gmail.com>
2022-07-26 17:28:12 +0430

    Add card test added

--- version/v1/tests.py
+++ version/v1/tests.py
@@ -12,7 +12,7 @@ class API_Test(APITestCase):
             First we create a user in our test db
         '''
         # create a new user
-        user = User.objects.create_user(
+        self.user = User.objects.create_user(
             email= "test@test.com",
             full_name= "user fullname",
             password= "TestPass1234"
@@ -31,3 +31,26 @@ class API_Test(APITestCase):
         access_key = dumped_data["access"]
         # authenticate via token
         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_key}')
+
+
+    def test_create_card(self):
+        '''
+            Create a new card for user.
+        '''
+
+        request = self.client.post(
+            reverse("v1:create-card"),
+            data={"password": "1234"},
+            format="json"
+        )
+
+        card = Card.objects.filter(owner= self.user).first()
+
+        # check if user have just one card
+        self.assertEqual(self.user.cards.count(), 1)
+        self.assertNotEqual(self.user.cards.count(), 2)
+
+        # check the balance of a card
+        self.assertEqual(card.balance, 0)
+        self.assertNotEqual(card.balance, 100)
+
