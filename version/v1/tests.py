69d00ad1b3602e9df332ca0984091caab6ecf674
Ml06py <liaghimohamad69@gmail.com>
2022-07-26 17:05:59 +0430

    Get user access token test added

--- version/v1/tests.py
+++ version/v1/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
+from django.urls import reverse
+from .models import Card
+import json
 
-# Create your tests here.
+from v1.models import User
+
+class API_Test(APITestCase):
+
+    def setUp(self):
+        '''
+            First we create a user in our test db
+        '''
+        # create a new user
+        user = User.objects.create_user(
+            email= "test@test.com",
+            full_name= "user fullname",
+            password= "TestPass1234"
+        )
+
+        # request for new api token
+        request = self.client.post(
+            reverse("token_obtain_pair"),
+            data={"email": "test@test.com", "password": "TestPass1234"},
+            format="json"
+        )
+
+        data = json.dumps(request.data)
+        dumped_data = json.loads(data)
+
+        access_key = dumped_data["access"]
+        # authenticate via token
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_key}')
