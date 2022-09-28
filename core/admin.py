2980d2ed07b3aa3a4da96c7af821d22eb1869976
Ml06py <liaghimohamad69@gmail.com>
2022-09-28 14:42:45 +0330

    Models added to admin page

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Card, Transaction, User
 
-# Register your models here.
+admin.site.register(Card)
+admin.site.register(Transaction)
+admin.site.register(User)
